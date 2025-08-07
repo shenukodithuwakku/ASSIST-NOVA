1d0800df4d16d4d52a1765c2560bb96bca288f7f
shenukodithuwakku <shenukodithuwakku760@gmail.com>
2025-08-07 21:27:33 +0530

    create interface

---
+++ www/main.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+    
+   $('.text').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bounceIn",
+        },
+        out: {
+            effect: "bounceOut",
+        },
+        
+   });
+
+
+});
+
+
+
